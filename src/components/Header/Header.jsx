ba857f2be72549b6a0fa86f07879cc3f63c9b72c
Arishem21 <162180068+Arishem21@users.noreply.github.com>
2024-07-01 20:23:51 +0700

    6436436

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,21 +6,33 @@ const Header = () => {
     <header className="header">
       <div className="header-left">
         <div className="logo">
-          <img src="path/to/logo.png" alt="Sociala Logo" />
+          <img className='Molniy' src="../../../public/image/Molniy.png" alt="Sociala Logo" />
           <span>Sociala.</span>
         </div>
         <input type="text" placeholder="Start typing to search.." />
       </div>
       <div className="header-icons">
-        <div className="icon active"><i className="home-icon" /></div>
-        <div className="icon"><i className="lightning-icon" /></div>
-        <div className="icon"><i className="video-icon" /></div>
-        <div className="icon"><i className="profile-icon" /></div>
-        <div className="icon"><i className="cart-icon" /></div>
+        <div className="icon active">
+            <img className='header-icon' src="../../../public/image/Home.png" alt="Home" />
+        </div>
+        <div className="icon active">
+            <img className='header-icon' src="../../../public/image/Lightning.png" alt="Lightning" />
+        </div>
+        <div className="icon active">
+            <img className='header-icon' src="../../../public/image/Video.png" alt="Video" />
+        </div>
+        <div className="icon active">
+        <img className='header-icon' src="../../../public/image/ProfilePicture.png" alt="Profile" />
+        </div>
+        <div className="icon active">
+        <img className='header-icon' src="../../../public/image/Cart.png" alt="Cart" />
+        </div>
       </div>
       <div className="header-right">
-        <div className="notification"><i className="bell-icon" /></div>
-        <div className="message"><i className="message-icon" /></div>
+      <img className='header-right' src="../../../public/image/Bell.png" alt="Bell" />
+      <img className='header-right' src="../../../public/image/Message.png" alt="Message" />
+      <img className='header-right' src="../../../public/image/Thems.png" alt="Thems" />
+      <img className='header-right' src="../../../public/image/ProFile.png" alt="ProFile" />
         <div className="profile"><i className="profile-picture" /></div>
       </div>
     </header>
